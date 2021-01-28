399fd706ea90b1866da60148c8c0de7617b04ad8
José Domingo Muñoz <josedom24@gmail.com>
2021-01-28 18:28:00 +0100

    cambio para que funciones el script

--- convert.sh
+++ convert.sh
@@ -1,5 +1,6 @@
-scp $1.rmd vagrant@192.168.121.27:
+ssh vagrant@192.168.121.27 "mkdir $1"
+scp $1/$2.rmd vagrant@192.168.121.27:$1
 scp -r include vagrant@192.168.121.27:
 scp -r img vagrant@192.168.121.27:
-ssh vagrant@192.168.121.27 "R -e \"rmarkdown::render('$1.rmd',output_file='$1.pdf')\""
-scp vagrant@192.168.121.27:$1.pdf .
+ssh vagrant@192.168.121.27 "cd $1 && R -e \"rmarkdown::render('$2.rmd',output_file='$2.pdf')\""
+scp vagrant@192.168.121.27:$1/$2.pdf $1
