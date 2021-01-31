ad90354c5bc9719fd2e880e65018e94400b43ef7
José Domingo Muñoz <josedom24@gmail.com>
2021-01-31 19:07:01 +0100

    imagenes docker

--- convert.sh
+++ convert.sh
@@ -5,6 +5,7 @@ USUARIO=vagrant
 
 ssh $USUARIO@$IP "mkdir $1"
 scp $1/$2.rmd $USUARIO@$IP:$1
+scp -r $1/img $USUARIO@$IP:$1/img
 scp -r include $USUARIO@$IP:
 scp -r img $USUARIO@$IP:
 ssh $USUARIO@$IP "cd $1 && R -e \"rmarkdown::render('$2.rmd',output_file='$2.pdf')\""
